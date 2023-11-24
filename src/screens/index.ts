88c57b220cf003afef8119f6d5a749a2045f16ba
Ubaid-0x <ubaid.iqbal@nextgeni.com>
2023-11-24 19:12:34 +0500

    scanner screen added

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,9 +1,6 @@
 import Login from './auth/login';
 import Home from './home';
 import Notification from './notification';
+import Scanner from './scanner';
 
-export {
-  Login,
-  Home,
-  Notification
-}
+export { Login, Home, Notification, Scanner };
